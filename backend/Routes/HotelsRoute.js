ae99d24d4722f0b098eb15b47b3e5d628a0f520b
Sachin Tewatia <tewatiasachin2001@gmail.com>
2023-06-16 22:32:52 +0530

    add day-3

--- backend/Routes/HotelsRoute.js
+++ backend/Routes/HotelsRoute.js
@@ -1,8 +1,47 @@
 const express = require("express");
+const { HotelModel } = require("../models/hotelModel");
 const hotelRoute = express.Router();
 
-hotelRoute.get("/",(req,res)=>{
-    res.send("hotel endpoint");
+// for getting
+hotelRoute.get("/",async(req,res)=>{
+    try {
+        const hotels =  await HotelModel.find();
+        res.status(200).send(hotels);
+    } catch (error) {
+        console.log(error);
+        res.status(400).send("error in getting hotels");
+    }
+})
+
+// "type":"DOSA specialist",
+//   "title":"Very good hotel in city",
+//   "desc":"kjhovidjiown",
+//   "city":"Palwal",
+//   "address":"Palwal",
+//   "distance":"300",
+//   "cheapestPrice":1000
+
+// for posting the new hotel
+hotelRoute.post("/add",async(req,res)=>{
+    const newHotel = new HotelModel(req.body);
+    try {
+        const savedHotel = await newHotel.save();
+        res.status(200).send({"msg":"New hotel added!"})
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({"msg":"Error in adding hotels???"})
+    }
+});
+
+// for updating 
+hotelRoute.patch("/update/:id",async(req,res)=>{
+    try {
+        const update = await HotelModel.findByIdAndUpdate({_id:req.params.id},req.body);
+        res.status(200).send({"successful":update})
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({"msg":"Error in updating"});
+    }
 })
 
 
